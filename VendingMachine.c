9069fb25a432bf6fb7e452c62ef206d9b2af5903
unknown <searain1110@gmail.com>
2026-01-30 11:02:28 +0900

    자판기 선택 오입력시 예외처리

--- VendingMachine.c
+++ VendingMachine.c
@@ -1,13 +1,16 @@
 ﻿#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+#include <stdbool.h>
+
+#define MENU 4		// 메뉴 가짓수
 
 void Print_Goods(char** chProducts, int* iPrices);		// 상품 출력
-void Select_Goods(char** chProducts, int* iPrices);		// 상품 선택, 고른 상품 가격 출력
+int Select_Goods(char** chProducts, int* iPrices);		// 상품 선택, 고른 상품 가격 출력
 
 int main(void)
 {
-	char* chProducts[] = { "콜라", "사이다", "초콜릿", "새콤달콤" };
-	int iPrices[] = { 1400, 1400, 1200, 1000 };
+	char* chProducts[MENU] = { "콜라", "사이다", "초콜릿", "새콤달콤" };
+	int iPrices[MENU] = { 1400, 1400, 1200, 1000 };
 
 	Print_Goods(&chProducts, &iPrices);
 	Select_Goods(&chProducts, &iPrices);
@@ -27,12 +30,30 @@ void Print_Goods(char** chProducts, int* iPrices)	// 이미 "콜라"같은게 ch
 	return 0;
 }
 
-void Select_Goods(char** chProducts, int* iPrices)		// 상품 선택
+int Select_Goods(char** chProducts, int* iPrices)		// 상품 선택
 {
 	int iSelect = 0;
 
-	printf("상품을 선택해주세요: ");
-	scanf_s("%d", &iSelect);
+	while (true)
+	{
+		printf("상품을 선택해주세요: ");
 
-	printf("\n[%s]울/를 선택하셨습니다. %d원입니다.\n", chProducts[iSelect], iPrices[iSelect]);
+		if (scanf_s("%d", &iSelect) == 1)			// 입력값이 정수라면
+		{
+			if (iSelect > MENU || iSelect < 0)		// 만약 선택이 범위에서 벗어난다면
+			{
+				printf("\n잘못된 입력입니다.\n");
+			}
+
+			else			// 선택 완료시
+			{
+				printf("\n[%s]울/를 선택하셨습니다. %d원입니다.\n", chProducts[iSelect], iPrices[iSelect]);
+				break;
+			}
+		}
+		else				// 입력값이 정수가 아닐 경우
+			while (getchar() != '\n');		// 버퍼 비우기
+	}
+
+	return iSelect;
 }
