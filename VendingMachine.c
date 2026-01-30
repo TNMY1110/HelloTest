c37df6caa33036eb32211e9af382c7799fb4c4cf
unknown <searain1110@gmail.com>
2026-01-30 13:12:16 +0900

    상품 재고 추가

--- VendingMachine.c
+++ VendingMachine.c
@@ -4,11 +4,12 @@
 
 #define MENU 4		// 메뉴 가짓수
 
-void Print_Goods(char** chProducts, int* iPrices);		// 상품 출력
-int Select_Goods(char** chProducts, int* iPrices);		// 상품 선택, 고른 상품 가격 출력
+void Print_Goods(char** chProducts, int* iPrices, int* iStock);		// 상품 출력
+void Check_Stock(char** chProducts, int* iPrices, int* iStock, int iMenu);	// 재고 여부에 따라 출력
+int Select_Goods(char** chProducts, int* iPrices, int* iStock);		// 상품 선택, 고른 상품 가격 출력
 bool Select_Purchase(bool isRestart);			// 구입 혹은 취소 선택
-int Insert_Money(int iPrice);		// 금액 투입
-void Purchase_Completed(char* chProducts);	// 구매 완료
+int Insert_Money(int iPrice);				// 금액 투입
+void Purchase_Completed(char* chProducts, int* iStock);	// 구매 완료
 void Get_Back_Change(int iChange);			// 잔돈 반환
 
 int main(void)
@@ -18,6 +19,7 @@ int main(void)
 
 	char* chProducts[MENU] = { "콜라", "사이다", "초콜릿", "새콤달콤" };	// 상품 이름
 	int iPrices[MENU] = { 1400, 1400, 1200, 1000 };						// 상품 가격
+	int iStock[MENU] = { 5, 4, 3, 2 };		// 상품 재고
 
 	int iSelect = 0;		// 선택한 상품 번호
 	int iChange = 0;		// 거스름돈
@@ -26,13 +28,13 @@ int main(void)
 	{
 		while (!isPurchase)		// 구매 선택
 		{
-			Print_Goods(&chProducts, &iPrices);
-			iSelect = Select_Goods(&chProducts, &iPrices);
+			Print_Goods(&chProducts, &iPrices, &iStock);
+			iSelect = Select_Goods(&chProducts, &iPrices, &iStock);
 			isPurchase = Select_Purchase(false);
 		}
 
 		iChange = Insert_Money(iPrices[iSelect]);
-		Purchase_Completed(chProducts[iSelect]);
+		Purchase_Completed(chProducts[iSelect], &iStock[iSelect]);
 		Get_Back_Change(iChange);
 
 		isPurchase = false;		// 구매 선택 초기화
@@ -42,19 +44,27 @@ int main(void)
 	return 0;
 }
 
-void Print_Goods(char** chProducts, int* iPrices)	// 이미 "콜라"같은게 char의 배열이라 chProducts는 배열의 배열이므로 이중 포인터로 매개변수 받기
+void Print_Goods(char** chProducts, int* iPrices, int* iStock)	// 이미 "콜라"같은게 char의 배열이라 chProducts는 배열의 배열이므로 이중 포인터로 매개변수 받기
 {
 	printf("=========메뉴=========\n");
-	printf("1.%s - %d원\n", chProducts[0], iPrices[0]);
-	printf("2.%s - %d원\n", chProducts[1], iPrices[1]);
-	printf("3.%s - %d원\n", chProducts[2], iPrices[2]);
-	printf("4.%s - %d원\n", chProducts[3], iPrices[3]);
+	Check_Stock(chProducts, iPrices, iStock, 0);
+	Check_Stock(chProducts, iPrices, iStock, 1);
+	Check_Stock(chProducts, iPrices, iStock, 2);
+	Check_Stock(chProducts, iPrices, iStock, 3);
 	printf("======================\n");
 
 	return 0;
 }
 
-int Select_Goods(char** chProducts, int* iPrices)		// 상품 선택
+void Check_Stock(char** chProducts, int* iPrices, int* iStock, int iMenu)
+{
+	if (iStock[iMenu] > 0)
+		printf("%d.%s - %d원 - %d개 남음\n", (iMenu + 1), chProducts[iMenu], iPrices[iMenu], iStock[iMenu]);
+	else
+		printf("%d.%s - 품절\n", (iMenu + 1), chProducts[iMenu]);
+}
+
+int Select_Goods(char** chProducts, int* iPrices, int* iStock)		// 상품 선택
 {
 	int iSelect = 0;
 
@@ -71,12 +81,22 @@ int Select_Goods(char** chProducts, int* iPrices)		// 상품 선택
 
 			else			// 선택 완료시
 			{
-				printf("\n[%s]을/를 선택하셨습니다. %d원입니다.\n", chProducts[iSelect - 1], iPrices[iSelect - 1]);
-				break;
+				if (iStock[iSelect - 1] <= 0)		// 재고가 0보다 작거나 같을 시
+				{
+					printf("\n[%s]의 재고가 부족합니다.\n", chProducts[iSelect - 1]);
+				}
+				else
+				{
+					printf("\n[%s]을/를 선택하셨습니다. %d원입니다.\n", chProducts[iSelect - 1], iPrices[iSelect - 1]);
+					break;
+				}
 			}
 		}
 		else				// 입력값이 정수가 아닐 경우
+		{
+			printf("\n잘못된 입력입니다.\n");
 			while (getchar() != '\n');		// 버퍼 비우기
+		}
 	}
 
 	return iSelect - 1;		// 선택이 1~4이므로 1 빼주기
@@ -151,10 +171,11 @@ int Insert_Money(int iPrice)		// 금액 투입
 	return iMoney - iPrice;		// 넣은 돈에서 가격만큼 빼고 반환
 }
 
-void Purchase_Completed(char* chProducts)
+void Purchase_Completed(char* chProducts, int* iStock)
 {
 	printf("\n=== 구매 완료 ===\n");
 	printf("[%s]이/가 나옵니다.\n", chProducts);
+	*iStock -= 1;
 }
 
 void Get_Back_Change(int iChange)
